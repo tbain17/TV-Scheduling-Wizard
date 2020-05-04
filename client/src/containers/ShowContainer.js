ef3f371a00a4b670cf09e3fcb5c39a9f698b1a69
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-04 12:03:16 +0100

    const url in container

--- client/src/containers/ShowContainer.js
+++ client/src/containers/ShowContainer.js
@@ -4,8 +4,14 @@ class ShowContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      schedule: [],
       shows: [],
+      selectedShow: ''
     }
+  }
+
+  componentDidMount() {
+    const url = 'http://api.tvmaze.com/schedule/full';
   }
 }
 export default ShowContainer;
