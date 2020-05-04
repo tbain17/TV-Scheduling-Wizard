cefc1573d06b9c90040d9c4d686b9b30c85d80cf
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-04 14:51:12 +0100

    const days

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -3,10 +3,11 @@ import React from 'react';
 export default Show;
 
 const Show = ({show, onShowDelete,}) => {
+  const days = {show.schedule.days.map((day) => {return (day)})}
   return(
     <div className="Show">
     <h3>{show.name}</h3>
-    <p>{show.schedule.days.map((day) => {return (day)})}</p>
+    <ul>{days}</ul>
     <p>{show.schedule.time}</p>
     <button onClick={() => onShowDelete(show._id)}>Delete</button>
     </div>
