a4cfed8aa1deaa63dd41a408d5775fa2d584e326
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-04 12:00:58 +0100

    class ShowContainer in container

--- client/src/containers/ShowContainer.js
+++ client/src/containers/ShowContainer.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
+class ShowContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shows: [],
+    }
+  }
+}
 export default ShowContainer;
