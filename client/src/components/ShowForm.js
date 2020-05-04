64cf22d91f0287e5c20351d2673d781953eb6a1e
Steve Maddison <stevemaddison@steves-mbp.home>
2020-05-04 16:54:00 +0100

    Add ShowForm.js

--- client/src/components/ShowForm.js
+++ client/src/components/ShowForm.js
@@ -1,3 +1,20 @@
 import React from 'react';
 
+const ShowForm = (props) => {
+  const options = props.shows.map(show => {
+    return <option value={show.schedule.time} key={show.id}>{show.schedule.time}</option>
+  })
+
+  function handleChange(event) {
+    props.onShowSelected(event.target.value);
+  }
+
+  return (
+    <select id="show-form" onChange={handleChange} defaultValue="default">
+      <option disabled value="default">Choose a Time</option>
+      {options}
+    </select>
+  )
+}
+
 export default ShowForm;
