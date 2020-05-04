65fccda4e8ef4cdb351473b8065dfa11610a7cdb
mike <>
2020-05-04 14:25:47 +0100

    Show List added

--- client/src/components/ShowList.js
+++ client/src/components/ShowList.js
@@ -1,3 +1,18 @@
 import React from 'react';
 
+const ShowList = ({ shows, onShowDelete }) => {
+  const showNodes = ShowList.map(show => (
+    <Show
+    key={show._id}
+    show={show}
+    onShowDelete={onShowDelete}></Show>
+  ));
+
+  return (
+    <div id="showList">
+    {showNodes}
+    </div>
+  )
+}
+
 export default ShowList;
