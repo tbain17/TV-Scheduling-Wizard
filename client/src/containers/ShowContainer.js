da48a8147202811a14b2b2b0b7d5d9cd176badfc
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-04 12:05:04 +0100

    render const selectedShow

--- client/src/containers/ShowContainer.js
+++ client/src/containers/ShowContainer.js
@@ -6,12 +6,20 @@ class ShowContainer extends React.Component {
     this.state = {
       schedule: [],
       shows: [],
-      selectedShow: ''
+      selectedShowName: ''
     }
   }
 
   componentDidMount() {
-    const url = 'http://api.tvmaze.com/schedule/full';
+    const url = 'http://api.tvmaze.com/shows';
+
+    fetch(url)
+    .then(shows => this.setState({ shows: shows}))
+    .catch(err => console.err);
+  }
+
+  render() {
+    const selectedShow = this.state.shows.find(show => show.name === this.state.selectedShowName)
   }
 }
 export default ShowContainer;
