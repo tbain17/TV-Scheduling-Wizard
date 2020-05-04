64e25376e87c516ff5ce89732301e6258479f084
Tamara Bain <tamara@tamaras-mbp.lan>
2020-05-04 14:09:13 +0100

    Added show.js file to models folder

---
+++ client/src/Models/show.js
@@ -0,0 +1,24 @@
+class Show {
+  static url = "http://localhost:3000/api/shows";
+
+  static get() {
+    return fetch(this.url)
+      .then(res => res.json());
+  }
+
+  static post(showData) {
+    return fetch(this.url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(showData)
+    }).then(res => res.json());
+  }
+
+  static delete(id) {
+    return fetch(`${this.url}/${id}`, {
+      method: 'DELETE'
+    }).then(res => res.json());
+  }
+}
+
+export default Show;
