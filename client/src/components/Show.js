ae92f33aaf1dfc5691fe1923a3084f80887c7c0f
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-04 14:38:23 +0100

    onclick delete

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -6,8 +6,9 @@ const Show = ({show, onShowDelete,}) => {
   return(
     <div className="Show">
     <h3>{show.name}</h3>
-    <p>{show.day}</p>
-    <p>{show.time}</p>
-    
-  )
-}
+    <p>{show.schedule.days[0]}</p>
+    <p>{show.schedule.time}</p>
+    <button onClick={() => onShowDelete(show._id)}>Delete</button>
+    </div>
+  );
+};
