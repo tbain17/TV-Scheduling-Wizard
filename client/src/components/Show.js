6f730c9a86367ceafa7304ece322b5f2d89acb33
Tamara Bain <tamara@tamaras-mbp.lan>
2020-05-05 09:53:32 +0100

    Added ShowForm time dropdown(unique values only) removed delete button from show item

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -6,7 +6,6 @@ const Show = ({show, onShowDelete,}) => {
     <h3>{show.name}</h3>
     <h3>{show.schedule.days.map((day) => {return `${day} `})}</h3>
     <p>{show.schedule.time}</p>
-    <button onClick={() => onShowDelete(show._id)}>Delete</button>
     </div>
   );
 };
--- client/src/components/ShowForm.js
+++ client/src/components/ShowForm.js
@@ -1,3 +1,26 @@
 import React from 'react';
 
+  const ShowForm = (props) => {
+    let uniqueTimes = []
+    props.shows.map(show => {
+      if (!uniqueTimes.includes(show.schedule.time)) (
+        uniqueTimes = [...uniqueTimes, show.schedule.time]
+  )})
+
+    const options = uniqueTimes.map(time => {
+      return <option value={time} >{time}</option>
+    })
+
+  function handleChange(event) {
+    props.onShowSelected(event.target.value);
+  }
+
+  return (
+    <select id="show-selector" onChange={handleChange} defaultValue="default">
+      <option disabled value="default">Choose a time...</option>
+      {options}
+    </select>
+  )
+}
+
 export default ShowForm;
