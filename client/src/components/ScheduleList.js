0a66bb9dcd5b140db5092ec36ea2613fbc7d80d8
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-05 11:42:27 +0100

    scheduleItem.js

--- client/src/components/ScheduleList.js
+++ client/src/components/ScheduleList.js
@@ -1,3 +1,19 @@
 import React from 'react';
+import ScheduleItem from './ScheduleItem';
+
+const ScheduleList = ({ schedule, onScheduleDelete }) => {
+  const scheduleItemNodes = schedule.map(schedule => (
+    <ScheduleItem
+    key={scheduleItem._id}
+    show={scheduleItem}
+    onShowAdded={onScheduleDelete}></ScheduleItem>
+  ));
+
+  return (
+    <div id="scheduleList">
+    {scheduleItemNodes}
+    </div>
+  )
+}
 
 export default ScheduleList;
