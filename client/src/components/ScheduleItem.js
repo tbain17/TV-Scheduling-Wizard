078cecf7136f849aeb15174a1cfe9ab15c35604b
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-05 11:59:27 +0100

    scheduleItem fix

--- client/src/components/ScheduleItem.js
+++ client/src/components/ScheduleItem.js
@@ -3,10 +3,10 @@ import React from 'react';
 const ScheduleItem = ({scheduleItem, onScheduleDelete}) => {
   return(
     <div className="Schedule">
-    <h3>{schedule.name}</h3>
-    <h3>{schedule.schedule.days.map((day) => {return `${day} `})}</h3>
-    <p>{schedule.schedule.time}</p>
-    <button onClick={() => onScheduleDelete(schedule._id)}>Remove from Schedule</button>
+    <h3>{scheduleItem.name}</h3>
+    <h3>{scheduleItem.schedule.days.map((day) => {return `${day} `})}</h3>
+    <p>{scheduleItem.schedule.time}</p>
+    <button onClick={() => onScheduleDelete(scheduleItem._id)}>Remove from Schedule</button>
     </div>
   );
 };
