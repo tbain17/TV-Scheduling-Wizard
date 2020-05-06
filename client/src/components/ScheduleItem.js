ea81de51617a7b2830044e966474e9ae87e985ba
Tamara Bain <tamara@tamaras-mbp.lan>
2020-05-06 14:25:38 +0100

    Added warning for time clashes

--- client/src/components/ScheduleItem.js
+++ client/src/components/ScheduleItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const ScheduleItem = ({scheduleItem, onScheduleDelete}) => {
+const ScheduleItem = ({scheduleItem, onScheduleDelete, clashMessage}) => {
   return(
     <div className="schedule">
     <h3>{scheduleItem.name}</h3>
     <p>{scheduleItem.schedule.days.map((day) => {return `${day} `})}</p>
     <p>{scheduleItem.schedule.time}</p>
     <p>{scheduleItem.network?.name}</p>
+    <p>{clashMessage}</p>
     <button onClick={() => onScheduleDelete(scheduleItem._id)}>Remove from Schedule</button>
     </div>
   );
--- client/src/components/ScheduleList.js
+++ client/src/components/ScheduleList.js
@@ -3,12 +3,28 @@ import ScheduleItem from './ScheduleItem';
 import '../containers/Showcontainer.css';
 
 const ScheduleList = ({ schedule, onScheduleDelete }) => {
-  const scheduleItemNodes = schedule.map(scheduleItem => (
-    <ScheduleItem
-    key={scheduleItem._id}
-    scheduleItem={scheduleItem}
-    onScheduleDelete={onScheduleDelete}></ScheduleItem>
-  ));
+  function getClashes (scheduleItem, schedule) {
+    return schedule.filter(item => {
+      return item.schedule.days.filter(itemDay => {
+        return scheduleItem.schedule.days.includes(itemDay)
+      }).length && item.schedule.time === scheduleItem.schedule.time && scheduleItem.id !== item.id;
+    });
+  }
+
+  const scheduleItemNodes = schedule.map(scheduleItem => {
+    const clashMessage = (getClashes(scheduleItem, schedule).length) ?
+      "This show clashes with another one in your schedule!" : null;
+
+    return (
+      <ScheduleItem
+      key={scheduleItem._id}
+      scheduleItem={scheduleItem}
+      onScheduleDelete={onScheduleDelete}
+      clashMessage={clashMessage}
+      >
+      </ScheduleItem>
+    )
+  });
 
   return (
     <div id="scheduleList" className="schedule-list">
