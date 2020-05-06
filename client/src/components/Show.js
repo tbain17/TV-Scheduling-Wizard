618c345eaf421af87250cc8d99d260b0b1e76324
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-06 10:10:32 +0100

    img in show.js

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -5,6 +5,7 @@ const Show = ({show, onShowAdded}) => {
   return(
     <div className="Show">
     <h3><a href={show.url}>{show.name}</a></h3>
+    <img src={show.image.medium} alt="">
     <h3>{show.schedule.days.map((day) => {return `${day} `})}</h3>
     <p>{show.schedule.time}</p>
     <button onClick={() => onShowAdded(show)}>Add to Schedule</button>
