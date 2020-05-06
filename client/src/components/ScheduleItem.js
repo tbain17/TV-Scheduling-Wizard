29193777d341c11ca1beb47344036e799c55a447
Steve Maddison <stevemaddison@steves-mbp.home>
2020-05-06 19:57:56 +0100

    align day and tine in scheduleitem.js

--- client/src/components/ScheduleItem.js
+++ client/src/components/ScheduleItem.js
@@ -4,8 +4,7 @@ const ScheduleItem = ({scheduleItem, onScheduleDelete, clashMessage}) => {
   return(
     <div className="schedule">
     <h3>{scheduleItem.name}</h3>
-    <p>{scheduleItem.schedule.days.map((day) => {return `${day} `})}</p>
-    <p>{scheduleItem.schedule.time}</p>
+    <p>{scheduleItem.schedule.days.map((day) => {return `${day} `})} {scheduleItem.schedule.time}</p>
     <p>{scheduleItem.network?.name}</p>
     <p className="red">{clashMessage}</p>
     <button onClick={() => onScheduleDelete(scheduleItem._id)}>Remove from Schedule</button>
