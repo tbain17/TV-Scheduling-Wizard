f35b009f6e3ad1eb2ee8b5d94788b32849e81ef1
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-06 10:16:18 +0100

    summary in show.js

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -6,6 +6,7 @@ const Show = ({show, onShowAdded}) => {
     <div className="Show">
     <h3><a href={show.url}>{show.name}</a></h3>
     <img src={show.image.medium} alt=""/>
+    <h4>{show.summary}</h4>
     <h3>{show.schedule.days.map((day) => {return `${day} `})}</h3>
     <p>{show.schedule.time}</p>
     <button onClick={() => onShowAdded(show)}>Add to Schedule</button>
