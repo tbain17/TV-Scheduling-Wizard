8f4787127625c1c4cd839d56d55d93efcfa3a60a
Shaun Jerdan <s.jerdan93@gmail.com>
2020-05-06 10:05:40 +0100

    href name link

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Show = ({show, onShowAdded}) => {
   return(
     <div className="Show">
-    <h3>{show.name}</h3>
+    <h3><a href={show.url}>{show.name}</a></h3>
     <h3>{show.schedule.days.map((day) => {return `${day} `})}</h3>
     <p>{show.schedule.time}</p>
     <button onClick={() => onShowAdded(show)}>Add to Schedule</button>
